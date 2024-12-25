48b64e97ece83238d4c138c088247c4919a25472
Nagarjunareddy <nagarjunareddy9995@gmail.com>
2024-12-25 20:24:34 +0530

    Create Archives and Compression.sh
    About Archives and Compression

---
+++ Archives and Compression.sh
@@ -0,0 +1,6 @@
+tar cf file.tar files #Creates a tsr named file.tar containing files
+tar xf file.tar #Extract the files from file.tar
+gzip file #Compresses file and renames it to file.gz
+gzip -d file.gz #Decompresses file.gz back to file
+zip -r file.zip files #Creates a zip archive named file.zip
+unzip file.zip #Extract the contents of a zip file
