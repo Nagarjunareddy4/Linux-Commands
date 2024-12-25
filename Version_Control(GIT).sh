393aaf9d630c1d1d0bd1262cc389179b015d21f3
Nagarjunareddy <nagarjunareddy9995@gmail.com>
2024-12-25 23:53:33 +0530

    Create Version_Control(GIT).sh
    This is about version control GIT.

---
+++ Version_Control(GIT).sh
@@ -0,0 +1,18 @@
+git init #Initialize a local git repository
+git clone url #Create a local copy of a remote repository
+git add filename #Add a file to the staging area
+git commit -m "Commit message" #Commit chages with a message
+git status #Check the status of the working directory
+git pull #Pull latest changes from the remote repository
+git push #push changes to the remote repository
+git branch #List all the local branches
+git branch branchname #Create a new branch
+git checkout branchname #Switch to a branch
+git merge branchname #Merge a branch into the active branch
+git stash #Stash changes a working directory
+git stash apply #Apply changes from a stash
+git log #View comment history
+git reset #Reset your head pointer to a previous commit
+git rm filename #Remove a file from version control
+git rebase #Reapply commits on top of another base tip
+git revert #Creates a new commit that undoes all the changes made in a particular commit, then apply it to the current branch.
