bb7f72f4ca3c690fa2dfa3d1c842ad30bdfd3ecf
Nagarjunareddy <nagarjunareddy9995@gmail.com>
2024-12-25 20:18:32 +0530

    Create Networking.sh
    about network commands in linux

---
+++ Networking.sh
@@ -0,0 +1,13 @@
+ping host #Ping a host and outputs results
+whois domain #Get whois information for domain
+dig domain #Get DNS information for domain
+netstat -pnltu #Displays various network related information such as network connections, routing tables, interface statistics etc.
+ifconfig #Displays IP address of all network interfaces
+ssh user@host #Remote login into the host as a user
+scp #Transfers files between hosts over ssh
+wget url #Download files from the web
+curl url #Sends a request to a URL and returns the response
+traceroute domain #prints the route that a pocket takes to reach the domain
+mtr domain #mtr combines the functionality of the traceroute and ping programs in a single network diagnostic tool.
+ss #Another utility to investigate sockets. It's a more modern alternative to netstat.
+nmap #Network exploration tool and security scanner.
